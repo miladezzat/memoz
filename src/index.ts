ac341a76917b35a397944b5e6f17f40c1b1db4fa
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-07-30 17:47:37 +0200

    updated method

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import getOneObject from './utils/get-one';
-import deleteData from './utils/delete';
 import uuid from './utils/uuid';
-import update from './utils/update';
-import updateOne from './utils/update-one';
-import getMany from './utils/get-many';
-import deleteOne from './utils/delete-one';
 import isObject from './utils/is-object';
+import isValidUUid from './utils/is-valid-uuid';
+import getOne from './utils/get-one';
+import getMany from './utils/get-many';
 
 /**
  * @description memoz is an in-memory database that persists on disk.
@@ -13,10 +10,10 @@ import isObject from './utils/is-object';
  * Strings, Lists, Sets, Sorted Sets, Hashes, Streams, HyperLogLogs, Bitmaps.
  */
 class Memoz {
-  private db: any[];
+  private db: any;
 
   constructor() {
-    this.db = [];
+    this.db = new Map();
   }
 
   /**
@@ -24,77 +21,187 @@ class Memoz {
    * @param document must be a javascript object with keys value pair
    * @returns {Object}
    */
-  public create(document:any): any {
+  public createOne(document:any): any {
     if (!isObject(document)) {
       throw new Error('the document must be a valid object');
     }
 
-    const dbDocument = {
-      id: uuid(),
-      ...document,
-    };
+    const id = uuid();
 
-    this.db.push(dbDocument);
+    const dbDocument = { ...document, id };
+
+    this.db.set(id, dbDocument);
 
     return dbDocument;
   }
 
-  /**
-   *
-   * @param query
-   * @returns {Object}
-   */
-  public get(query?:any) {
-    if (query && !isObject(query)) {
-      throw new Error('the query must be a valid object');
+  public createMany(documents:any[]): any[] {
+    documents.map((document) => {
+      if (!isObject(document)) {
+        throw new Error('the document must be a valid object');
+      }
+
+      return document;
+    });
+
+    return documents.map((document) => this.createOne(document));
+  }
+
+  public getById(id: string): any {
+    if (!isValidUUid(id)) {
+      throw new Error('the document must be a valid object');
     }
 
-    if (!query || !Object.keys(query).length) {
-      return this.db;
+    return this.db.get(id);
+  }
+
+  public getOne(query: any): any {
+    if (!isObject(query)) {
+      throw new Error('The query must be a valid object');
     }
 
-    return getMany(query, this.db);
+    return getOne(query, [...this.db.values()]);
   }
 
-  public getOne(query:any) {
-    const result = getOneObject(query, this.db);
+  public getMany(query: any): any {
+    if (!isObject(query)) {
+      throw new Error('The query must be a valid object');
+    }
 
-    return result;
+    return getMany(query, [...this.db.values()]);
   }
 
-  public deleteMany(query:any) {
-    const results = deleteData(query, this.db);
+  public updateById(id:string, newData:any) {
+    if (!isValidUUid(id)) {
+      throw new Error('the document must be a valid object');
+    }
 
-    this.db = results;
+    if (!isObject(newData)) {
+      throw new Error('The new data must be a valid object');
+    }
 
-    return { deleted: true, number: this.db.length - results.length };
+    const oneObject = this.getById(id);
+
+    if (!oneObject) {
+      throw new Error('This id not exists');
+    }
+
+    const newObject = { ...oneObject, ...newData };
+
+    this.db.set(id, newObject);
+
+    return newObject;
   }
 
-  public deleteOne(query:any) {
-    const deleted = this.getOne(query);
-    const results = deleteOne(deleted, this.db);
+  public updateOne(query:any, newData: any) {
+    if (!isObject(query)) {
+      throw new Error('The query must be a valid object');
+    }
 
-    this.db = results;
+    if (!isObject(newData)) {
+      throw new Error('The new data must be a valid object');
+    }
 
-    return { deleted };
+    const oneObject = this.getOne(query);
+
+    if (!oneObject) {
+      throw new Error('This query not match anything');
+    }
+
+    const newObject = { ...oneObject, ...newData };
+
+    this.db.set(oneObject.id, newObject);
+
+    return newObject;
   }
 
   public updateMany(query:any, newData:any) {
-    const results = update(query, this.db, newData);
+    if (!isObject(query)) {
+      throw new Error('The query must be a valid object');
+    }
 
-    this.db = results;
+    if (!isObject(newData)) {
+      throw new Error('The new data must be a valid object');
+    }
 
-    return { updated: true };
+    let documents = this.getMany(query);
+
+    documents = documents.map((document:any) => this.updateById(document.id, newData));
+
+    return { updated: true, n: documents.length, documents };
   }
 
-  public updateOne(query:any, newData:any) {
-    const obj = this.getOne(query);
+  public deleteById(id: string): any {
+    if (!isValidUUid(id)) {
+      throw new Error('the document must be a valid object');
+    }
 
-    const { updated, updatedData } = updateOne(obj, this.db, newData);
+    const deletedObject: any = this.getById(id);
+    this.db.delete(id);
 
-    this.db = updatedData;
+    return deletedObject;
+  }
 
-    return { updated: true, data: updated };
+  public deleteOne(query:any): any {
+    if (!isObject(query)) {
+      throw new Error('The query must be a valid object');
+    }
+
+    const deletedObject: any = this.getOne(query);
+
+    return this.deleteById(deletedObject.id);
+  }
+
+  public deleteMany(query:any) {
+    if (!isObject(query)) {
+      throw new Error('The query must be a valid object');
+    }
+    const documents = this.getMany(query);
+
+    documents.map((document:any) => this.deleteById(document.id));
+
+    const sizeAfterDeleted = this.countDocuments(query);
+
+    if (sizeAfterDeleted) {
+      return { deleted: false };
+    }
+
+    return { deleted: true, n: documents.length };
+  }
+
+  public deleteAll() {
+    const size = this.countDocuments();
+
+    this.db.clear();
+
+    if (this.countDocuments()) {
+      return { deleted: false, n: size };
+    }
+
+    return { deleted: true, n: size };
+  }
+
+  public countDocuments(query?:any) {
+    if (query && !isObject(query)) {
+      throw new Error('The query must be a valid object');
+    }
+
+    if (query && Object.keys(query)) {
+      const documents:any[] = this.getMany(query);
+      return documents.length;
+    }
+
+    return this.db.size;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public id():string {
+    return uuid();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public isValidUUID(id:string):boolean {
+    return isValidUUid(id);
   }
 }
 
